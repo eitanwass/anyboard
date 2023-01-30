95e222fb0019948f78c9cc7cff37e78f19c3f767
Ethan <eitanwass@gmail.com>
2023-01-30 11:21:10 +0200

    Move board stuff to correct component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import React, {useCallback, useMemo, useState} from 'react';
+import React, {useState} from 'react';
 import './App.sass';
 import Board from "./Components/Board";
-import {Stage} from "react-konva";
-import {SurfaceFactory} from "./Components/Surface";
-import _ from "lodash";
-import {TokenFactory} from "./Components/Token";
 
 
 type CreationOption = "Token" | "Surface";
@@ -14,50 +10,11 @@ const App = () => {
 
 	const [selectedOption, setSelectedOption] = useState<CreationOption>("Surface");
 
-	const [boardItems, setBoardItems] = useState<JSX.Element[]>([]);
-	const [previewCreation, setPreviewCreation] = useState<({type: any, [key: string]: string}) | undefined>();
-
-	const onCreateBoardItem = useCallback(
-		(newBoardItem: JSX.Element) => {
-			setBoardItems((current) => [...current, newBoardItem]);
-		},
-		[setBoardItems]
-	);
-
-	const creationPreviewInstance = useMemo(() =>
-		previewCreation ?
-			<previewCreation.type {..._.omit(previewCreation, ["type"])}/> :
-			undefined,
-		[previewCreation]
-	);
-
-	const surfaceMouseEvents = useMemo(() => SurfaceFactory(
-		onCreateBoardItem,
-		(e) => setPreviewCreation(e)),
-		[onCreateBoardItem, setPreviewCreation]
-	);
-
-	const tokenMouseEvents = useMemo(() => TokenFactory(
-		onCreateBoardItem,
-		(e) => setPreviewCreation(e)),
-		[onCreateBoardItem, setPreviewCreation]
-	);
-
 	return (
 		<div className="App">
-			<Stage
-				width={window.innerWidth}
-				height={window.innerHeight}
-				onMouseDown={(e) => tokenMouseEvents.onMouseDown(e)}
-				onMouseMove={(e) => tokenMouseEvents.onMouseMove(e)}
-				onMouseUp={(e) => tokenMouseEvents.onMouseUp(e)}
-			>
-				<Board
-					boardItems={boardItems}
-					previewBoardItems={creationPreviewInstance}
-					resolution={resolution}
-				/>
-			</Stage>
+			<Board
+				resolution={resolution}
+			/>
 		</div>
 	);
 };
--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -1,14 +1,43 @@
-import {Circle, Layer} from "react-konva";
-import React, {useMemo} from "react";
+import {Circle, Layer, Stage} from "react-konva";
+import React, {useCallback, useMemo, useState} from "react";
 import _ from "lodash";
+import {SurfaceFactory} from "./Surface";
+import {TokenFactory} from "./Token";
 
 type boardType = {
-	boardItems: JSX.Element[],
-	previewBoardItems: JSX.Element | undefined,
 	resolution?: number,
 }
 
-const Board = ({ boardItems, previewBoardItems, resolution = 50 }: boardType) => {
+const Board = ({resolution = 50}: boardType) => {
+	const [boardItems, setBoardItems] = useState<JSX.Element[]>([]);
+	const [previewBoardItemData, setPreviewBoardItemData] = useState<({ type: any, [key: string]: string }) | undefined>();
+
+	const onCreateBoardItem = useCallback(
+		(newBoardItem: JSX.Element) => {
+			setBoardItems((current) => [...current, newBoardItem]);
+		},
+		[setBoardItems]
+	);
+
+	const creationPreviewInstance = useMemo(() =>
+			previewBoardItemData ?
+				<previewBoardItemData.type {..._.omit(previewBoardItemData, ["type"])}/> :
+				undefined,
+		[previewBoardItemData]
+	);
+
+	const surfaceMouseEvents = useMemo(() => SurfaceFactory(
+		onCreateBoardItem,
+		(e) => setPreviewBoardItemData(e)),
+		[onCreateBoardItem, setPreviewBoardItemData]
+	);
+
+	const tokenMouseEvents = useMemo(() => TokenFactory(
+		onCreateBoardItem,
+		(e) => setPreviewBoardItemData(e)),
+		[onCreateBoardItem, setPreviewBoardItemData]
+	);
+
 	const backgroundDots = useMemo(() => {
 		const dots = [];
 		for (let x = 0; x < window.innerWidth; x += resolution) {
@@ -20,12 +49,18 @@ const Board = ({ boardItems, previewBoardItems, resolution = 50 }: boardType) =>
 	}, [resolution, window.innerWidth, window.innerHeight]);
 
 	return (
-		<>
+		<Stage
+			width={window.innerWidth}
+			height={window.innerHeight}
+			onMouseDown={(e) => tokenMouseEvents.onMouseDown(e)}
+			onMouseMove={(e) => tokenMouseEvents.onMouseMove(e)}
+			onMouseUp={(e) => tokenMouseEvents.onMouseUp(e)}
+		>
 			<Layer name={"background"}>
 				{
 					_.map(
 						backgroundDots, (dot) =>
-						<Circle x={dot.x} y={dot.y} width={2} height={2} fill={"black"}/>
+							<Circle x={dot.x} y={dot.y} width={2} height={2} fill={"black"}/>
 					)
 				}
 			</Layer>
@@ -33,9 +68,9 @@ const Board = ({ boardItems, previewBoardItems, resolution = 50 }: boardType) =>
 				{boardItems}
 			</Layer>
 			<Layer name={"preview-board-items"}>
-				{previewBoardItems}
+				{creationPreviewInstance}
 			</Layer>
-		</>
+		</Stage>
 	);
 };
 
